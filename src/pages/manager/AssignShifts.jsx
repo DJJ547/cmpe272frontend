789d79cd76b053656a389156cf04fe30ce1d652e
Jiajun Dai <jdai10@mail.ccsf.edu>
2023-12-06 11:34:45 -0800

    removed unused libs

--- src/pages/manager/AssignShifts.jsx
+++ src/pages/manager/AssignShifts.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
-import "react-time-picker/dist/TimePicker.css";
 
 
 const AssignShifts = () => {
